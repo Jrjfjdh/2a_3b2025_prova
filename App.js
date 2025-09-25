380ca35ee542a3fd7bb12dc1d2a2251fb8ea233c
Gabriel Müller Ferreira Neves <gabrielmnf2311@gmail.com>
2025-09-25 11:37:17 -0300

    Finalizar a prova de perfil

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>2°ano</Text>
+      <Text style={styles.texto}>3°Bimestre</Text>
+      <Text style={styles.texto}>Gabriel Müller</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto: {
+    fontSize: 20,
+    marginBottom: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 5,
+  }
+});
